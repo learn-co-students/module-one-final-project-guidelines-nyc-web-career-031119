f52bc8d837e1288aae712536ce59015d18c524b7
whosAle <abelgrave95@gmail.com>
2019-03-27 09:33:35 -0400

    seeded more questionmaster instances

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,8 +4,8 @@ jim = Player.create(name: jim)
 catwoman = Player.create(name: catwoman)
 duckowl = Player.create(name: duckowl)
 
-5.times { Question.create (get_random_question_from_api)}
+15.times { Question.create_or_find_by (get_random_question_from_api)}
 
-QuestionMaster.create(jim.id, Question.all.sample.id)
-QuestionMaster.create(catwoman.id, Question.all.sample.id)
-QuestionMaster.create(duckowl.id, Question.all.sample.id)
+2.times {QuestionMaster.create(jim.id, Question.all.sample.id)}
+2.times {QuestionMaster.create(catwoman.id, Question.all.sample.id)}
+2.times {QuestionMaster.create(duckowl.id, Question.all.sample.id)}
